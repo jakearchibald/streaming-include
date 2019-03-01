626e917461f3a8f04118774458542959b1ddbe23
Jake Archibald <jaffathecake@gmail.com>
2019-03-01 14:57:42 +0000

    Adding tricky tests

--- polyfill/index.js
+++ polyfill/index.js
@@ -16,10 +16,15 @@ class ParserChunk {
   }
 }
 
+/**
+ * @extends {TransformStream<string, ParserChunk>}
+ */
 export class DOMParserStream extends TransformStream {
   constructor() {
     /** @type {CloneMap<Node>} */
     const cloneMap = new WeakMap();
+    /** @type {Set<Node>} */
+    const removedNodes = new Set();
     const doc = document.implementation.createHTMLDocument();
     const cloneStartPoint = document.createElement('template').content;
     doc.write('<template>');
@@ -33,20 +38,35 @@ export class DOMParserStream extends TransformStream {
      * @param {Node} node
      * @param {Node | null} parent
      * @param {Node | null} nextSibling
-     * @returns {ParserChunk}
      */
-    function getChunkForNode(node, parent, nextSibling) {
+    function handleAddedNode(node, parent, nextSibling) {
       if (!cloneMap.has(node)) {
         const clone = node.cloneNode();
         cloneStartPoint.append(clone);
         cloneMap.set(node, clone);
       }
 
-      return new ParserChunk(
-        /** @type {Node} */ (cloneMap.get(node)),
-        !parent || parent === root ? null : /** @type {Node} */ (cloneMap.get(parent)),
-        !nextSibling ? null : /** @type {Node} */ (cloneMap.get(nextSibling))
+      controller.enqueue(
+        new ParserChunk(
+          /** @type {Node} */ (cloneMap.get(node)),
+          !parent || parent === root ? null : /** @type {Node} */ (cloneMap.get(parent)),
+          !nextSibling ? null : /** @type {Node} */ (cloneMap.get(nextSibling))
+        )
       );
+
+      // If this node has reappeared after an earlier removal, remove it from the set.
+      const removedNodeFromSet = removedNodes.delete(node);
+
+      // Otherwise, we check to see if one of the removed nodes has reappeared in this one (due to a parsing error)
+      if (!removedNodeFromSet) {
+        for (const removedNode of removedNodes) {
+          if (removedNode.parentNode === node) {
+            handleAddedNode(removedNode, removedNode.parentNode, removedNode.nextSibling);
+            // Exit. The recursive call will take care of removing this item, and rechecking the set.
+            return;
+          }
+        }
+      }
     }
 
     /**
@@ -54,27 +74,35 @@ export class DOMParserStream extends TransformStream {
      * @param {string} oldText
      * @param {Node | null} parent
      * @param {Node | null} nextSibling
-     * @returns {ParserChunk}
      */
-    function getChunkForTextChange(node, oldText, parent, nextSibling) {
+    function handleChangedText(node, oldText, parent, nextSibling) {
+      if (parent && parent.nodeType === 3) debugger;
       const additionalText = new Text(node.data.slice(oldText.length));
       cloneStartPoint.append(additionalText);
 
-      return new ParserChunk(
-        additionalText,
-        !parent || parent === root ? null : /** @type {Node} */ (cloneMap.get(parent)),
-        !nextSibling ? null : /** @type {Node} */ (cloneMap.get(nextSibling))
+      controller.enqueue(
+        new ParserChunk(
+          additionalText,
+          !parent || parent === root ? null : /** @type {Node} */ (cloneMap.get(parent)),
+          !nextSibling ? null : /** @type {Node} */ (cloneMap.get(nextSibling))
+        )
       );
     }
 
     new MutationObserver((entries) => {
       for (const entry of entries) {
-        console.log('node', entry.addedNodes[0], 'parent', entry.target, 'removed', entry.removedNodes[0], 'nextSib', entry.nextSibling);
-        for (const node of entry.addedNodes) controller.enqueue(getChunkForNode(node, entry.target, entry.nextSibling));
+        // console.log('node', entry.addedNodes[0], 'parent', entry.target, 'removed', entry.removedNodes[0], 'nextSib', entry.nextSibling);
+        for (const node of entry.removedNodes) {
+          // Nodes are removed during parse errors, but will reappear later. They may be inserted
+          // into a node that isn't currently in the document, so it won't reappear in addedNodes,
+          // so we need to cater for that.
+          removedNodes.add(node);
+        }
+        for (const node of entry.addedNodes) {
+          handleAddedNode(node, entry.target, entry.nextSibling);
+        }
         if (entry.type == 'characterData') {
-          controller.enqueue(
-            getChunkForTextChange(/** @type {Text} */ (entry.target), /** @type {string} */ (entry.oldValue), entry.target, entry.nextSibling)
-          );
+          handleChangedText(/** @type {Text} */ (entry.target), /** @type {string} */ (entry.oldValue), entry.target.parentNode, entry.nextSibling)
         }
       }
     }).observe(root, {
@@ -91,7 +119,29 @@ export class DOMParserStream extends TransformStream {
       },
       flush() {
         doc.close();
-        console.log('parsed', root);
+        // console.log('parsed', root);
+      }
+    });
+  }
+}
+
+/**
+ * @extends {WritableStream<ParserChunk>}
+ */
+export class DOMWritable extends WritableStream {
+  /**
+   * @param {Element} target
+   */
+  constructor(target) {
+    super({
+      write({node, nextSibling, parent}) {
+        // If we're adding a text node next to another text node, combine their values
+        const previousSibling = nextSibling ? nextSibling.previousSibling : (parent || target).lastChild;
+        if (node.nodeType === 3 && previousSibling && previousSibling.nodeType === 3) {
+          /** @type {string} */ (previousSibling.nodeValue) += node.nodeValue;
+        } else {
+          (parent || target).insertBefore(node, nextSibling);
+        }
       }
     });
   }
--- polyfill/test/index.js
+++ polyfill/test/index.js
@@ -1,26 +1,68 @@
 import '../../node_modules/mocha/mocha.js';
 import '../../node_modules/chai/chai.js';
 
-import { DOMParserStream } from '../index.js'
+import { DOMParserStream, DOMWritable } from '../index.js'
 
 mocha.setup('tdd');
 
-const { assert: {
-} } = chai;
+const { assert } = chai;
 
-suite('Basic tests', () => {
-  test('Test', async () => {
-    const transform = new DOMParserStream();
-    window.writer = transform.writable.getWriter();
+function createElementWritable(el) {
+  const transform = new DOMParserStream();
+  transform.readable.pipeTo(new DOMWritable(el));
+  return transform.writable;
+}
 
-    const reader = transform.readable.getReader();
+function parseHTMLTest(name, html) {
+  test(name, async () => {
+    const targetInnerHTML = document.createElement('div');
+    const targetOneChunkStream = document.createElement('div');
+    const targetMultiChunkStream = document.createElement('div');
 
-    while (true) {
-      const { done, value } = await reader.read();
-      if (done) return;
-      console.log('From readable', value);
+    targetInnerHTML.innerHTML = html;
+
+    {
+      const writer = createElementWritable(targetOneChunkStream).getWriter();
+      await writer.write(html);
+      writer.close();
     }
+
+    {
+      const writer = createElementWritable(targetMultiChunkStream).getWriter();
+      for (const char of html) {
+        await writer.write(char);
+      }
+      writer.close();
+    }
+
+    assert.isTrue(targetInnerHTML.isEqualNode(targetOneChunkStream), 'targetOneChunkStream');
+    assert.isTrue(targetInnerHTML.isEqualNode(targetMultiChunkStream), 'targetMultiChunkStream');
   });
+}
+
+suite('Tricky tests', () => {
+  // These are from https://github.com/html5lib/html5lib-tests/blob/master/tree-construction/tricky01.dat
+  parseHTMLTest('1', '<b><p>Bold </b> Not bold</p>Also not bold.');
+  parseHTMLTest('2', `<font color=red><i>Italic and Red<p>Italic and Red </font> Just italic.</p> Italic only.</i> Plain
+<p>I should not be red. <font color=red>Red. <i>Italic and red.</p>
+<p>Italic and red. </i> Red.</font> I should not be red.</p>
+<b>Bold <i>Bold and italic</b> Only Italic </i> Plain`);
+  parseHTMLTest('3', `<dl>
+<dt><b>Boo
+<dd>Goo?
+</dl>`);
+  parseHTMLTest('4', `<label><a><div>Hello<div>World</div></a></label>`);
+  parseHTMLTest('5', `<table><center> <font>a</center> <img> <tr><td> </td> </tr> </table>`);
+  parseHTMLTest('6', `<table><tr><p><a><p>You should see this text.`);
+  parseHTMLTest('7', `<TABLE>
+<TR>
+<CENTER><CENTER><TD></TD></TR><TR>
+<FONT>
+<TABLE><tr></tr></TABLE>
+</P>
+<a></font><font></a>`);
+  parseHTMLTest('8', `<b><nobr><div>This text is in a div inside a nobr</nobr>More text that should not be in the nobr, i.e., the
+nobr should have closed the div inside it implicitly. </b><pre>A pre tag outside everything else.</pre>`);
 });
 
 mocha.run();
